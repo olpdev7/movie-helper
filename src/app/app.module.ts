4e24681c9ba82e99ade15ac0fce5825919c6675d
Oleh Pavlov <olpdev7@gmail.com>
2018-02-27 18:00:33 +0200

    Added angular material hub module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MaterialHubModule } from './material-hub/material-hub.module';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialHubModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material-hub/material-hub.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { MatListModule } from '@angular/material';
+
+@NgModule({
+  imports: [MatListModule],
+  exports: [MatListModule]
+})
+export class MaterialHubModule { }
